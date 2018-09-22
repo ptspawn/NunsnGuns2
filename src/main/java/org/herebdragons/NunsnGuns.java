5cdb8ac87101f989f030c20d5213f0aaf7932c9f
ptspawn <ptspawn@gmail.com>
2018-09-23 00:05:13 +0100

    window on

---
+++ src/main/java/org/herebdragons/NunsnGuns.java
@@ -0,0 +1,16 @@
+package org.herebdragons;
+
+import org.herebdragons.graphics.Canvas;
+import org.herebdragons.graphics.enums.WindowBehaviour;
+
+import java.awt.*;
+
+public class NunsnGuns {
+
+    public static void main(String[] args) {
+
+        Dimension dimension = new Dimension(800,600);
+        Canvas canvas = Canvas.createCanvas("Teste",new Dimension(800,600), WindowBehaviour.EXIT_ON_CLOSE);
+
+    }
+}
---
+++ src/main/java/org/herebdragons/engine/GameEngine.java
@@ -0,0 +1,8 @@
+package org.herebdragons.engine;
+
+public class GameEngine implements Runnable {
+
+    public void run() {
+
+    }
+}
